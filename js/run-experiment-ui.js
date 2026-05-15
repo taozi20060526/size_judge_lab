95073172693e5457ff5a07b26b190188246dc7e8
taozi20060526 <2858083813@qq.com>
2026-05-15 10:26:18 +0800

    修复 T/C 未定义导致无法开始会话

--- js/run-experiment-ui.js
+++ js/run-experiment-ui.js
@@ -5,6 +5,8 @@
  * 依赖前述各 window.SJL_* 模块；本文件不写试次算法、不写持久化细节，只负责交互与状态机。
  */
 (function () {
+  const C = window.SJL_CONFIG;
+  const T = window.SJL_TRIALS;
   const S = window.SJL_STORAGE;
   const ST = window.SJL_STATS;
   const R = window.SJL_REMOTE;
